db365ca495faa9df79cbcc14ac8a076fc45e628b
Mithun Balaji <mithunb986@gmail.com>
2024-07-24 23:52:39 +0530

    Remove Element

---
+++ arrays-string/removeElement.java
@@ -0,0 +1,17 @@
+package com.mithun.leetcode75.arraysstrings;
+import java.util.*;
+public class removeElement {
+    public static void main(String[] args) {
+        int[] nums = {3,2,2,3};
+        int val =3;
+        int count = 0;
+        List<Integer> arr = new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+
+            if(!(nums[i]==val)){
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+    }
+}
