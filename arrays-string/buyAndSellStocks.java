43d31741f4d0113307826dcf7353fedb7c176248
Mithun Balaji <mithunb986@gmail.com>
2024-07-24 23:08:50 +0530

    Added Best time to buy and sell stock

---
+++ arrays-string/buyAndSellStocks.java
@@ -0,0 +1,14 @@
+package com.mithun.leetcode;
+
+public class buyAndSellStocks {
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        int buy = prices[0];
+        int profit = 0;
+        for(int i: prices){
+            buy = Math.min(buy,i);
+            profit = Math.max(profit,i-buy);
+        }
+        System.out.println(profit);
+    }
+}
