0b7389e70a9a9b71e526a375cf3ecb100748c783
Mithun Balaji <69361233+mithun1302@users.noreply.github.com>
2024-08-01 22:25:15 +0530

    Create rotateArray.java

---
+++ arrays-string/rotateArray.java
@@ -0,0 +1,17 @@
+package com.mithun.leetcode150.arraysstring;
+public class rotateArray {
+    public static void main(String[] args) {
+       int[] nums = {1,2,3,4,5,6,7};
+       int k = 3;
+       int[] rotate = new int[nums.length];
+       for(int i=0;i<nums.length;i++){
+           rotate[(i+k)%nums.length] = nums[i];
+       }
+        for(int i=0;i<nums.length;i++){
+            nums[i] = rotate[i];
+        }
+        for(int i=0;i<nums.length;i++){
+            System.out.print(nums[i]+" ");
+        }
+    }
+}
